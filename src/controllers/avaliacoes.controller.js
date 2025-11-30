e2bab02fa8b75aca771f58dbef7d9474ec98ddab
Henry Diamantino <henry.diamantino74@gmail.com>
2025-11-30 02:59:25 -0300

    arrumando avaliacoes

--- src/controllers/avaliacoes.controller.js
+++ src/controllers/avaliacoes.controller.js
@@ -6,13 +6,13 @@ import { db } from "../config/db.js"
 
 export async function criarAvaliacao(req, res) {
     try {
-        const { nota, comentario } = req.body;
-        if (!nota || !comentario)
+        const { usuario_id, livro_id, nota, comentario } = req.body;
+        if (!usuario_id || !livro_id || !nota || !comentario)
             return res.status(400).json({ erro: "Campos obrigatórios" });
 
         await db.execute(
-            "INSERT INTO avaliacoes (nota, comentario) VALUES (?, ?)",
-            [nota, comentario]
+            "INSERT INTO avaliacoes (usuario_id, livro_id, nota, comentario) VALUES (?, ?, ?, ?)",
+            [usuario_id, livro_id, nota, comentario]
         );
 
         res.json({ mensagem: "Avaliação criada com sucesso!" });
@@ -24,7 +24,11 @@ export async function criarAvaliacao(req, res) {
 
 export async function listaAvaliacao(req, res) {
     try {
-        const [rows] = await db.execute("SELECT * FROM avaliacoes");
+        const [rows] = await db.execute(`
+            SELECT a.id, u.nome, l.titulo, a.nota, a.comentario FROM avaliacoes a
+            INNER JOIN usuarios u ON a.usuario_id = u.id
+            INNER JOIN livros l ON a.livro_id = l.id
+            `);
         res.json(rows);
     } catch (err) {
         res.status(500).json({ erro: err.message });
@@ -34,9 +38,12 @@ export async function listaAvaliacao(req, res) {
 
 export async function obterAvaliacao(req, res) {
     try {
-        const [rows] = await db.execute("SELECT * FROM avaliacoes WHERE id = ?", [
-            req.params.id,
-        ]);
+        const [rows] = await db.execute(`
+            SELECT a.id, u.nome, l.titulo, a.nota, a.comentario FROM avaliacoes a
+            INNER JOIN usuarios u ON a.usuario_id = u.id
+            INNER JOIN livros l ON a.livro_id = l.id 
+            WHERE id = ?`, 
+            [req.params.id,]);
         if (rows.length === 0)
             return res.status(404).json({ erro: "Avaliação não encontrada" });
         res.json(rows[0]);
@@ -47,10 +54,10 @@ export async function obterAvaliacao(req, res) {
 
 export async function atualizarAvaliacao(req, res) {
     try {
-        const { nota, comentario } = req.body;
+        const { usuario_id, livro_id, nota, comentario } = req.body;
         await db.execute(
-            "UPDATE avaliacao SET nota = ?, comentario = ? WHERE id = ?",
-            [nota, comentario, req.params.id]
+            "UPDATE avaliacoes SET usuario_id = ?, livro_id = ?, nota = ?, comentario = ? WHERE id = ?",
+            [usuario_id, livro_id, nota, comentario, req.params.id]
         );
         res.json({ mensagem: "Avaliação atualizada com sucesso!" });
     } catch (err) {
@@ -61,7 +68,7 @@ export async function atualizarAvaliacao(req, res) {
 
 export async function deletarAvaliacao(req, res) {
     try {
-        await db.execute("DELETE FROM avalicao WHERE id = ?", [req.params.id]);
+        await db.execute("DELETE FROM avaliacoes WHERE id = ?", [req.params.id]);
         res.json({ mensagem: "Avaliação deletada com sucesso!" });
     } catch (err) {
         res.status(500).json({ erro: err.message });
